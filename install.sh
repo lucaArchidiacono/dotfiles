025e52ec8968e13dc38c8c492b73589702e8fdf3
lucaArchidiacono <mail@archiluc.ch>
2025-03-24 17:55:14 +0100

    create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Install git
+brew install git
+
+# Install bash-completion -> used for autocompletion
+brew install bash-completion
+
+# Install gh -> command line tool from github
+brew install gh
+
+# Install fuzzy search tool
+brew install fzf
+
+# Install Aerospace window manager
+brew brew install --cask nikitabobko/tap/aerospace
+
+# Install ghostty terminal
+brew install --cask ghostty
+
+# Install zoxide -> smarter `cd`
+brew install zoxide
+
+# Install new fonts (better looking yazi)
+brew install font-symbols-only-nerd-font
+
+# Install FFMPEG -> always needed
+brew install ffmpeg
+
+# Install for yazi (Finder inside Terminal) - and dependencies
+brew install yazi sevenzip jq poppler fd ripgrep imagemagick 
+
+# Install bun (faster JS runtime)
+brew install oven-sh/bun/bun
+
+# Install fast Node Manager
+brew install fnm
+
+# Install atuin
+brew install atuin
